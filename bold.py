708bd774f75b98b33b90de41c130a1579c84f6c6
Muhammad Suhaib <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-05-20 20:11:34 +0500

    cleaned

--- bold.py
+++ bold.py
@@ -7,23 +7,45 @@ red_end = '\033[0m'
 
 class accessModifiers:
     def __init__(self):
+###############################################################################################
+#                                   SOME ATTRIBUTES
+############################################################################################### 
         self.public_variable = "I am public variable"
         self._protected_variable = "I am protected variable"
         self.__private_variable = "I am private variable"
-        # self.public_method()
-        # self._protected_method()
-        # self.__private_method()
-        # self.__private_method_with_args("Hello", "World")
-        # self.__private_method_with_args("Hello", "World", "!")
-        # self.__private_method_with_args("Hello", "World", "!", "Python")
+
+###############################################################################################
+#                                   SOME METHODS
+############################################################################################### 
+
+    def public_method(self):
+        print("I am public method")
+
+    def _protected_method(self):
+        print("I am protected method")
+
+    def __private_method(self):
+        print("I am private method")
+
+###############################################################################################
+#                                   GETTER AND SETTER
+############################################################################################### 
 
     def get_private_variable(self):
         return self.__private_variable
+    def get_private_method(self):
+        return self.__private_method()
     def set_private_attribute(self,new_variable):
         if type(new_variable)!= str: return(f'{red_start}NAN{red_end}') 
         else :
-            self.new_variable = self.__private_variable 
+            self.__private_variable = new_variable
             return new_variable
+
+
+
+###############################################################################################
+#                                    ATTRIBUTE CALLS
+###############################################################################################
 
 lets_access = accessModifiers()
 print(lets_access.public_variable)
@@ -31,12 +53,23 @@ print('PROTECTED DON`T SHY TO SHOW OFF >>',lets_access._protected_variable)
 print('ACCESS BY NAME MANGLED >>',lets_access._accessModifiers__private_variable)
 print('ACCESS BY GETTER >>',lets_access.get_private_variable())
 
-#print(lets_access.__private_variable) # AttributeError: 'accessModifiers' object has no attribute '__private_variable'. 
+#print(lets_access.__private_variable) #❌❌ AttributeError: 'accessModifiers' object has no attribute '__private_variable'. 
 lets_access.__private_variable = 'i `ve updated the private attribute'
-print( lets_access.__private_variable)
+print( lets_access.__private_variable)# creates new public var, doesn't affect the private one
 print('>>',lets_access.__private_variable is lets_access.get_private_variable()) #false 
-# this means that i ve only updated the instance`s attribute but class still ve same
 
 print('ACCESS BY SETTER >>',lets_access.set_private_attribute('updated using setter'))
 print('ACCESS BY SETTER >>',lets_access.set_private_attribute(3))
 print('ACCESS BY GETTER >>',lets_access.get_private_variable())
+
+###############################################################################################
+#                                    METHODS CALLS
+###############################################################################################
+
+obj = accessModifiers()
+obj.public_method()
+obj._protected_method()
+obj._accessModifiers__private_method() # access private method using name mangling
+obj.get_private_method() # access private method using getter
+
+#obj.__private_method() # ❌❌AttributeError: 'accessModifiers' object has no attribute '__private_method'
