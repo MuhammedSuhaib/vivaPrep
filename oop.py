542fff4a2534433952b43264abe7a9e548208804
Muhammad Suhaib <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-06-18 19:01:27 +0500

    decorators

--- oop.py
+++ oop.py
@@ -111,3 +111,54 @@ print(f"Is Alice an adult? {Person.is_adult(person1.age)}")  # Output: Is Alice 
 # All other classes are **children** (directly or indirectly) of it.
 
 # Unified data type simply means everything is an object.
+
+# `@property` makes a method act like an attribute. Same as getter
+
+# ### Example:
+
+# ```py
+# class Person:
+#     def __init__(self, name):
+#         self._name = name
+
+#     @property
+#     def name(self):
+#         return self._name
+
+# p = Person("Ali")
+# print(p.name)  # acts like attr, calls method
+# ```
+
+# No `()` needed when accessing. Good for read-only or controlled access.
+
+
+        #A decorator is a function that wraps another function to modify its behavior.
+
+### `@property`, `@<prop>.setter`, `@<prop>.deleter` example:
+
+# ```py
+# class A:
+#     def __init__(self):
+#         self._x = 1
+
+#     @property
+#     def x(self):
+#         return self._x
+
+#     @x.setter
+#     def x(self, val):
+#         self._x = val
+
+#     @x.deleter
+#     def x(self):
+#         del self._x
+# ```
+
+# Use:
+
+# ```py
+# a = A()
+# a.x = 5       # setter
+# print(a.x)    # getter
+# del a.x       # deleter
+# ```
